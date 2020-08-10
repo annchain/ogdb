31fa52e6b4c59ef00477922f700521d3e90aa77c
Uni <boh_zhu@163.com>
2020-08-10 18:32:32 +0800

    init memdb and leveldb

---
+++ memdb/memdb_test.go
@@ -0,0 +1,142 @@
+package memdb
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"sync"
+	"testing"
+)
+
+func TestMemoryDB_PutGet(t *testing.T) {
+	testPutGet(NewMemDatabase(), t)
+}
+
+var test_values = []string{"", "a", "1251", "\x00123\x00"}
+
+func testPutGet(db *MemDatabase, t *testing.T) {
+	t.Parallel()
+
+	for _, v := range test_values {
+		err := db.Put([]byte(v), []byte(v))
+		if err != nil {
+			t.Fatalf("put failed: %v", err)
+		}
+	}
+
+	for _, v := range test_values {
+		data, err := db.Get([]byte(v))
+		if err != nil {
+			t.Fatalf("get failed: %v", err)
+		}
+		if !bytes.Equal(data, []byte(v)) {
+			t.Fatalf("get returned wrong result, got %q expected %q", string(data), v)
+		}
+	}
+
+	for _, v := range test_values {
+		err := db.Put([]byte(v), []byte("?"))
+		if err != nil {
+			t.Fatalf("put override failed: %v", err)
+		}
+	}
+
+	for _, v := range test_values {
+		data, err := db.Get([]byte(v))
+		if err != nil {
+			t.Fatalf("get failed: %v", err)
+		}
+		if !bytes.Equal(data, []byte("?")) {
+			t.Fatalf("get returned wrong result, got %q expected ?", string(data))
+		}
+	}
+
+	for _, v := range test_values {
+		orig, err := db.Get([]byte(v))
+		if err != nil {
+			t.Fatalf("get failed: %v", err)
+		}
+		orig[0] = byte(0xff)
+		data, err := db.Get([]byte(v))
+		if err != nil {
+			t.Fatalf("get failed: %v", err)
+		}
+		if !bytes.Equal(data, []byte("?")) {
+			t.Fatalf("get returned wrong result, got %q expected ?", string(data))
+		}
+	}
+
+	for _, v := range test_values {
+		err := db.Delete([]byte(v))
+		if err != nil {
+			t.Fatalf("delete %q failed: %v", v, err)
+		}
+	}
+
+	for _, v := range test_values {
+		_, err := db.Get([]byte(v))
+		if err == nil {
+			t.Fatalf("got deleted value %q", v)
+		}
+	}
+}
+
+func TestMemoryDB_ParallelPutGet(t *testing.T) {
+	testParallelPutGet(NewMemDatabase(), t)
+}
+
+func testParallelPutGet(db *MemDatabase, t *testing.T) {
+	const n = 8
+	var pending sync.WaitGroup
+
+	pending.Add(n)
+	for i := 0; i < n; i++ {
+		go func(key string) {
+			defer pending.Done()
+			err := db.Put([]byte(key), []byte("v"+key))
+			if err != nil {
+				panic("put failed: " + err.Error())
+			}
+		}(strconv.Itoa(i))
+	}
+	pending.Wait()
+
+	pending.Add(n)
+	for i := 0; i < n; i++ {
+		go func(key string) {
+			defer pending.Done()
+			data, err := db.Get([]byte(key))
+			if err != nil {
+				panic("get failed: " + err.Error())
+			}
+			if !bytes.Equal(data, []byte("v"+key)) {
+				panic(fmt.Sprintf("get failed, got %q expected %q", []byte(data), []byte("v"+key)))
+			}
+		}(strconv.Itoa(i))
+	}
+	pending.Wait()
+
+	pending.Add(n)
+	for i := 0; i < n; i++ {
+		go func(key string) {
+			defer pending.Done()
+			err := db.Delete([]byte(key))
+			if err != nil {
+				panic("delete failed: " + err.Error())
+			}
+		}(strconv.Itoa(i))
+	}
+	pending.Wait()
+
+	pending.Add(n)
+	for i := 0; i < n; i++ {
+		go func(key string) {
+			defer pending.Done()
+			_, err := db.Get([]byte(key))
+			if err == nil {
+				panic("get succeeded")
+			}
+		}(strconv.Itoa(i))
+	}
+	pending.Wait()
+}
